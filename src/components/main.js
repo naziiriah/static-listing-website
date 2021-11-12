a496fba5b3373d7ad4058e38cbd46a3696a0e25f
naziiriah <77500215+naziiriah@users.noreply.github.com>
2021-11-12 20:11:06 +0100

    updated: rectify the display

--- src/components/main.js
+++ src/components/main.js
@@ -17,15 +17,15 @@ return(
              .filter(jobs=> !level ? jobs.level : jobs.level === level)
              .filter(jobs=> !tool[0] ? jobs.tools : jobs.tools[0] === tool[0])
              .map(jobs=>{
-                !role && !lang[0] && !level && !tool[0] ? console.log(false) : <div className="table">
-                {role ===""? console.log("is empty") : <div className="tab">{role}</div>}
-                {lang[0] === "" ? console.log("is empty") : <div className="tab">{lang[0]}</div>}
-                {level === "" ? console.log("is empty") : <div className="tab">{level}</div>}
-                {tool[0] === "" ? console.log("is empty") : <div className="tab">{tool[0]}</div>}
-                </div>
-                jobs.featured ? document.getElementById('info').style.borderLeft = "#fff": document.getElementById('info').style.borderLeft = "10px hsl(180, 8%, 52%) solid"
-                jobs.featured ? document.querySelector('.featured').style.display = "none" :document.querySelector('.featured').style.display = "block"
-                jobs.new ? document.querySelector('.new').style.display = "none" : document.querySelector('.new').style.display = "block"
+                // !role && !lang[0] && !level && !tool[0] ? console.log(false) : <div className="table">
+                // {role ===""? console.log("is empty") : <div className="tab">{role}</div>}
+                // {lang[0] === "" ? console.log("is empty") : <div className="tab">{lang[0]}</div>}
+                // {level === "" ? console.log("is empty") : <div className="tab">{level}</div>}
+                // {tool[0] === "" ? console.log("is empty") : <div className="tab">{tool[0]}</div>}
+                // </div>
+                // jobs.featured ? document.getElementById('info').style.borderLeft = "#fff": document.getElementById('info').style.borderLeft = "10px hsl(180, 8%, 52%) solid"
+                // jobs.featured ? document.querySelector('.featured').style.display = "none" :document.querySelector('.featured').style.display = "block"
+                // jobs.new ? document.querySelector('.new').style.display = "none" : document.querySelector('.new').style.display = "block"
                 const job = jobs.languages
                     return(
                         
